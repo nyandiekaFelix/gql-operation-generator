d4f318cf4631709cbfabe86dd35cad4a59d97df1
nyandiekaFelix <nyandiekafelix@gmail.com>
2018-10-15 12:58:16 +0300

    chore(setup): update linter configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,10 @@
 module.exports = {
   extends: 'airbnb',
-  plugins: ['react', 'import'],
+  plugins: ['import'],
   parser: 'babel-eslint',
   env: {
     browser: true,
     node: true,
     jest: true
   },
-  rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/no-children-prop': 0,
-    'no-unused-expressions': ["error", {"allowShortCircuit": true}],
-    'class-methods-use-this': ["error", {"exceptMethods": ['getSnapshotBeforeUpdate', 'render', 'componentWillUnmount', 'componentDidMount',]}],
-  }
 };
