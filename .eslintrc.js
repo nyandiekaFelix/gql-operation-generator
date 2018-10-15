381b10b5a6b32c4af5ae16848c7225127b27a467
nyandiekaFelix <nyandiekafelix@gmail.com>
2018-10-15 12:49:56 +0300

    chore(setup): initialize project configurations

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: 'airbnb',
+  plugins: ['react', 'import'],
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true,
+    jest: true
+  },
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/no-children-prop': 0,
+    'no-unused-expressions': ["error", {"allowShortCircuit": true}],
+    'class-methods-use-this': ["error", {"exceptMethods": ['getSnapshotBeforeUpdate', 'render', 'componentWillUnmount', 'componentDidMount',]}],
+  }
+};
